36aae0896c4b70c01af8c791de270486b862ff87
Denny Britz <dennybritz@gmail.com>
2015-04-11 14:02:48 +0900

    Update DB directly

--- app.js
+++ app.js
@@ -10,19 +10,14 @@ if(!process.env.MONGODB_URI){
 }
 mongoose.connect(process.env.MONGODB_URI);
 
-if(!process.env.LINKEDIN_EMAIL){
-  console.log('You must set the LINKEDIN_EMAIL environment varible.');
-}
-if(!process.env.LINKEDIN_PASSWORD){
-  console.log('You must set the LINKEDIN_PASSWORD environment varible.');
-}
-
-mongoose.set('debug', true);
-
 var runAsync = function(){
   return app.updatePeopleAsync().then(function(){
     return Promise.delay(30000).then(runAsync);
   });
 };
 
-app.loginAsync(process.env.LINKEDIN_EMAIL, process.env.LINKEDIN_PASSWORD).then(runAsync);
+if(process.env.LINKEDIN_EMAIL && process.env.LINKEDIN_PASSWORD){
+  app.loginAsync(process.env.LINKEDIN_EMAIL, process.env.LINKEDIN_PASSWORD).then(runAsync); 
+} else {
+  runAsync();
+}
